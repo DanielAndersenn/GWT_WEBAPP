87be46b2412044b9fcdf4ca8079cf10fc9e4f583
Rasmus Edvardsen <rasmus.edvardsen@hotmail.com>
2016-06-12 16:13:36 +0200

    throws new afspuodgvr

--- GWT_WEBAPP/src/com/grp13/webapp/server/dao/implementation/UserImplementation.java
+++ GWT_WEBAPP/src/com/grp13/webapp/server/dao/implementation/UserImplementation.java
@@ -15,7 +15,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class UserImplementation implements UserInterface{
-	
+
 	private Connection conn;
 	private Statement stm;
 	private List<UserDTO> uList;
@@ -23,11 +23,11 @@ public class UserImplementation implements UserInterface{
 	int returnMsg = 0;
 	String SQLMsg = "";
 	int SQLErr = 0;
-	
+
 	public UserImplementation(Connection conn) {
 		this.conn = conn;
 	}
-	
+
 	@Override
 	public UserDTO getUser(String name) throws DALException {
 		// TODO Auto-generated method stub
@@ -39,7 +39,7 @@ public class UserImplementation implements UserInterface{
 		System.out.println("getUserList START");
 		uList = new ArrayList<UserDTO>();
 		String sql = "SELECT * FROM user";
-		
+
 		try {
 			stm = conn.createStatement();
 			ResultSet rs = stm.executeQuery(sql);
@@ -49,11 +49,11 @@ public class UserImplementation implements UserInterface{
 		} catch(SQLException sqlE) {
 			System.out.println("SQL Exception occured: " + sqlE);
 		}
-		
+
 		System.out.println("getUserList END");
 		return uList;
 	}
-	
+
 
 	@Override
 	public void createUser(UserDTO user) throws DALException {
@@ -62,7 +62,7 @@ public class UserImplementation implements UserInterface{
 		String cmd = "CALL InsertUser(?, ?, ?, ?, ?, ?, ?)";
 		try {
 			stmt = conn.prepareCall(cmd);
-			
+
 			stmt.setString(1, user.getName());
 			stmt.setString(2, user.getInitials());
 			stmt.setString(3, user.getPassword());
@@ -70,13 +70,13 @@ public class UserImplementation implements UserInterface{
 			stmt.registerOutParameter(5, java.sql.Types.INTEGER);
 			stmt.registerOutParameter(6, java.sql.Types.VARCHAR);
 			stmt.registerOutParameter(7, java.sql.Types.SMALLINT);
-			
+
 			stmt.executeUpdate();
-			
+
 			returnMsg = stmt.getInt(5);
 			SQLMsg = stmt.getString(6);
 			SQLErr = stmt.getInt(7);
-			
+
 			System.out.println("var returnMsg: " + returnMsg + " var SQLMsg: " + SQLMsg + " var SQLErr: " + SQLErr);
 			System.out.println("createUser END");
 		} catch (SQLException e) {
@@ -91,10 +91,10 @@ public class UserImplementation implements UserInterface{
 		System.out.println("updateUser START");
 		CallableStatement stmt = null;
 		String cmd = "CALL UpdateUser(?, ?, ?, ?, ?, ?, ?, ?)";
-		
+
 		try {
 			stmt = conn.prepareCall(cmd);
-			
+
 			stmt.setInt(1, user.getUserID());
 			stmt.setString(2, user.getName());
 			stmt.setString(3, user.getInitials());
@@ -103,13 +103,13 @@ public class UserImplementation implements UserInterface{
 			stmt.registerOutParameter(6, java.sql.Types.INTEGER);
 			stmt.registerOutParameter(7, java.sql.Types.VARCHAR);
 			stmt.registerOutParameter(8, java.sql.Types.SMALLINT);
-			
+
 			stmt.executeUpdate();
-			
+
 			returnMsg = stmt.getInt(6);
 			SQLMsg = stmt.getString(7);
 			SQLErr = stmt.getInt(8);
-			
+
 			System.out.println("var returnMsg: " + returnMsg + " var SQLMsg: " + SQLMsg + " var SQLErr: " + SQLErr);
 			System.out.println("updateUser END");
 		} catch (SQLException e) {
@@ -124,28 +124,25 @@ public class UserImplementation implements UserInterface{
 		System.out.println("deleteUser START");
 		CallableStatement stmt = null;
 		String cmd = "CALL DeleteUser(?, ?, ?, ?)";
-		
+
 		try {
 			stmt = conn.prepareCall(cmd);
-			
+
 			stmt.setInt(1, a);
 			stmt.registerOutParameter(2, java.sql.Types.INTEGER);
 			stmt.registerOutParameter(3, java.sql.Types.VARCHAR);
 			stmt.registerOutParameter(4, java.sql.Types.SMALLINT);
-			
+
 			stmt.executeUpdate();
-			
+
 			returnMsg = stmt.getInt(2);
 			SQLMsg = stmt.getString(3);
 			SQLErr = stmt.getInt(4);
-			
+
 			System.out.println("var returnMsg: " + returnMsg + " var SQLMsg: " + SQLMsg + " var SQLErr: " + SQLErr);
 			System.out.println("deleteUser END");
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 			System.out.println("deleteUser FAIL");
 		}
 	}
-
 }
--- GWT_WEBAPP/src/com/grp13/webapp/shared/DALException.java
+++ GWT_WEBAPP/src/com/grp13/webapp/shared/DALException.java
@@ -5,4 +5,7 @@ public class DALException extends Exception
 	private static final long serialVersionUID = -5490114628089339322L;
 	public DALException(String message) { super(message); }    
 	public DALException(Exception e) { super(e); }
+	public DALException() {
+		System.out.println("Ender vi her?");
+	}
 }
