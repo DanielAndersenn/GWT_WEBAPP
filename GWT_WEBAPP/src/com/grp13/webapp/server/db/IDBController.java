f07da51ac96dda02f5315e1445d5a23e866a63f5
Rasmus Edvardsen <rasmus.edvardsen@hotmail.com>
2016-06-13 13:57:46 +0200

    praise be to yeezy

--- GWT_WEBAPP/src/com/grp13/webapp/server/db/IDBController.java
+++ GWT_WEBAPP/src/com/grp13/webapp/server/db/IDBController.java
@@ -1,10 +1,11 @@
 package com.grp13.webapp.server.db;
 
+import com.grp13.webapp.server.dao.interfaces.OpskriftTrinInterface;
 import com.grp13.webapp.server.dao.interfaces.UserInterface;
 import com.grp13.webapp.server.dao.interfaces.opskriftInterface;
 import com.grp13.webapp.shared.AccessDeniedException;
 
-public interface IDBController extends UserInterface, opskriftInterface {
+public interface IDBController extends UserInterface, opskriftInterface, OpskriftTrinInterface {
 
 	boolean validateUser(String userID, String password) throws AccessDeniedException;
 
